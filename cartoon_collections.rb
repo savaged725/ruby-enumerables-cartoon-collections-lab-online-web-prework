772b3c4eeda82606730727845935f1217fb87f9a
Deborah Savage <savaged725@yahoo.com>
2019-07-16 20:39:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves  ["Doc", "Dopey", "Bashful", "Grumpy"]
+  puts Doc 
+  puts Dopey
+  puts Bashful
+  puts Grumpy
 end
 
 def summon_captain_planet# code an argument here
